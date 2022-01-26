f598be45ac0fccc6c749b9de992531202f3ad2d2
Peter Simsik <peter.simsik@unilabs.sk>
2022-01-26 20:20:33 +0100

    Initial commit

---
+++ src/main/java/sk/simo/sdjpaintro/SdjpaIntroApplication.java
@@ -0,0 +1,13 @@
+package sk.simo.sdjpaintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaIntroApplication.class, args);
+	}
+
+}
