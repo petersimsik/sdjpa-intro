fb0892c3d40a280a5f6ffe7a3aa7ac229d19e891
Simo@HP <peter.simsik@gmail.com>
2022-02-23 08:58:26 +0100

    joa splice

---
+++ src/test/java/sk/simo/sdjpaintro/SpringBootJpaSplice.java
@@ -0,0 +1,39 @@
+package sk.simo.sdjpaintro;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import sk.simo.sdjpaintro.domain.Book;
+import sk.simo.sdjpaintro.repositories.BookRepository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DataJpaTest
+public class SpringBootJpaSplice {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    @Commit
+    @Order(1)
+    void testJpaTestSplice() {
+        long countBefore = bookRepository.count();
+        bookRepository.save(new Book("myBook","123456", "self"));
+        long countAfter = bookRepository.count();
+
+        assertThat(countBefore).isNotSameAs(countAfter);
+    }
+
+    @Test
+    @Order(2)
+    void testAnotherTest(){
+        long count = bookRepository.count();
+        assertThat(count).isEqualTo(1);
+    }
+}
