3f6214f9a1d8d207321476a0688da162814ab9f7
Simo@HP <peter.simsik@gmail.com>
2022-03-10 20:07:00 +0100

    refactor bookUuidTest and authorUuidTest merged into MySqlIntegrationTest

--- src/test/java/sk/simo/sdjpaintro/MySqlIntegrationTest.java
+++ src/test/java/sk/simo/sdjpaintro/MySqlIntegrationTest.java
@@ -1,5 +1,6 @@
 package sk.simo.sdjpaintro;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,13 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
+import sk.simo.sdjpaintro.domain.AuthorUuid;
+import sk.simo.sdjpaintro.domain.BookUuid;
+import sk.simo.sdjpaintro.repositories.AuthorUuidRepository;
 import sk.simo.sdjpaintro.repositories.BookRepository;
+import sk.simo.sdjpaintro.repositories.BookUuidRepository;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -18,6 +25,60 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class MySqlIntegrationTest {
     @Autowired
     BookRepository bookRepository;
+
+    @Autowired
+    BookUuidRepository bookUuidRepository;
+
+    @Autowired
+    AuthorUuidRepository authorUuidRepository;
+
+    @Test
+    void saveAuthorUuidTest() {
+        long countBefore = authorUuidRepository.count();
+        AuthorUuid author = new AuthorUuid();
+        author.setFirstName("Peter");
+        author.setLastName("Simsik");
+        AuthorUuid savedAuthor = authorUuidRepository.save(author);
+
+        Assertions.assertThat(countBefore).isNotEqualTo(authorUuidRepository.count());
+    }
+
+    @Test
+    void getAuthorUuidByIdTest() {
+        AuthorUuid author = new AuthorUuid();
+        author.setFirstName("Peter");
+        author.setLastName("Simsik");
+        AuthorUuid savedAuthor = authorUuidRepository.save(author);
+        UUID savedUUID = savedAuthor.getId();
+
+        AuthorUuid authorFromDb = authorUuidRepository.getById(savedUUID);
+        Assertions.assertThat(authorFromDb).isEqualTo(savedAuthor);
+
+    }
+
+    @Test
+    void saveBookUuidTest() {
+        Long countBefore = bookUuidRepository.count();
+        BookUuid book = new BookUuid();
+        book.setTitle("Moja pekna knizka");
+        book.setPublisher("Bla");
+        BookUuid savedBook = bookUuidRepository.save(book);
+
+        Assertions.assertThat(countBefore).isNotEqualTo(bookUuidRepository.count());
+    }
+
+    @Test
+    void getBookUuidByIdTest() {
+        BookUuid book = new BookUuid();
+        book.setTitle("Moja pekna knizka");
+        book.setPublisher("Bla");
+        BookUuid savedBook = bookUuidRepository.save(book);
+        UUID uuid = savedBook.getId();
+
+        BookUuid bookFromDb = bookUuidRepository.getById(uuid);
+
+        Assertions.assertThat(bookFromDb).isEqualTo(savedBook);
+    }
 
     @Test
     void testMySQL(){
