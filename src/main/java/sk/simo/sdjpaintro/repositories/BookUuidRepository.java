6128a1c0392ee65ae57bd5504ac15367a40e7a78
Peter Simsik <peter.simsik@unilabs.sk>
2022-03-10 16:52:44 +0100

    fixed repository, id is no longer Long but UUID

--- src/main/java/sk/simo/sdjpaintro/repositories/BookUuidRepository.java
+++ src/main/java/sk/simo/sdjpaintro/repositories/BookUuidRepository.java
@@ -3,5 +3,7 @@ package sk.simo.sdjpaintro.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sk.simo.sdjpaintro.domain.BookUuid;
 
-public interface BookUuidRepository extends JpaRepository<BookUuid, Long> {
+import java.util.UUID;
+
+public interface BookUuidRepository extends JpaRepository<BookUuid, UUID> {
 }
